825ad12bd59a16ce2faccefb6fbb28fc4fe075a0
bikem-dm <81683544+bikem-dm@users.noreply.github.com>
2021-05-16 15:56:21 +0300

    Add files via upload

---
+++ lib/screens/loginPage.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:login/textField.dart';
+import 'btn.dart';
+
+
+
+
+
+
+
+class google_button extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  SizedBox(height: MediaQuery.of(context).size.height * 0.07,
+      width: MediaQuery.of(context).size.width *0.8,
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(25)),
+        onPressed: () {},
+        color: Colors.white,
+        child: Text("Login with Google",  textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.deepPurple, fontSize:18,fontWeight: FontWeight.bold),),
+      ),
+    );
+  }
+}
+
+
+
+class login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  SingleChildScrollView(
+      child: SafeArea(
+        child: Column(children: [
+          Row(mainAxisAlignment:MainAxisAlignment.center,
+              children:[
+                SizedBox(height: MediaQuery.of(context).size.height * 0.4,
+                    width: MediaQuery.of(context).size.width *0.5,child: Image(image:AssetImage('assets/knowy.jpg')))] ),
+          Padding(
+            padding: EdgeInsets.only(),
+            child: textField("E-Mail",0.07,0.8,25,Colors.white),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top:MediaQuery.of(context).size.height*0.03,),
+            child: textField("Password",0.07,0.8,25,Colors.white),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top:MediaQuery.of(context).size.height*0.03,),
+            child: Btn("Login", Colors.deepPurple,0.07,0.8),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top:MediaQuery.of(context).size.height*0.03,),
+            child: google_button(),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top:MediaQuery.of(context).size.height*0.03,),
+            child: Row(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(onPressed: (){},
+                    child: Text("Forgot my Password",
+                      style: TextStyle(color: Colors.deepPurple, fontSize:15),)),
+
+              ],),
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(bottom: MediaQuery.of(context).size.height*0.05,),
+            child: Row(mainAxisAlignment: MainAxisAlignment.center,
+
+              children: [
+                Text("New around here?",style: TextStyle(fontSize:15),),
+                TextButton(onPressed: (){},
+                    child: Text("Sing up",
+                      style: TextStyle(color: Colors.deepPurple, fontSize:15),)),
+              ],
+            ),
+          ),
+        ],
+        ),
+      ),
+    );
+  }
+}
+
+
