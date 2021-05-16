369428c9db2961466b89d918181845b4b7b7c67b
bikem-dm <81683544+bikem-dm@users.noreply.github.com>
2021-05-16 15:54:37 +0300

    Update btn.dart

--- lib/useful_widgets/btn.dart
+++ lib/useful_widgets/btn.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
+
+
 class Btn extends StatelessWidget {
   final String buttonTxt;
   final Color foregroundColor;
- 
-  Btn(this.buttonTxt,this.foregroundColor);
+  final double buttonWidth;
+  final double buttonHeight;
+
+  Btn(this.buttonTxt, this.foregroundColor,this.buttonHeight, this.buttonWidth);
+  // heigth: 0.07  width: 0.8
+
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-                      onPressed: () {},
-                      color: foregroundColor,
-                      padding: EdgeInsets.symmetric(horizontal: 50),
-                      elevation: 2,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                      child: Text(
-                        buttonTxt,
-                        style: TextStyle(fontSize: 14, color: Colors.white),
-                      ),
-                    );
+    return SizedBox(height: MediaQuery.of(context).size.height * buttonHeight,
+      width: MediaQuery.of(context).size.width *buttonWidth,
+
+
+      child: RaisedButton(
+        onPressed: () {},
+        color: foregroundColor,
+        padding: EdgeInsets.symmetric(horizontal: 50),
+        elevation: 2,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25)),
+        child: Text(
+          buttonTxt,
+          style: TextStyle(fontSize: 14, color: Colors.white),
+        ),
+      ),
+    );
   }
-  
 }
